c6d46074fdbca9b3f47fabc06df6f384c5b51d89
eadehemingway <eade.hemingway@gmail.com>
2018-03-21 17:59:43 +0000

    addition to request function

--- public/dom.js
+++ public/dom.js
@@ -1,1 +1,22 @@
-console.log('im alive!');
+var inputField = document.querySelector(".input");
+
+inputField.addEventListener("keyup", function(e) {
+  var userInput = e.target.value;
+  }
+});
+
+
+var request = function(param){
+var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4){
+      if (xhr.readyState === 200){
+
+      }else {
+        console.log(error)
+      }
+    }
+  }
+  xhr.open("GET", url, true)
+  xhr.send()
+}
