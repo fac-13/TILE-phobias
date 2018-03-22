c4c3388fe0a1cc8d52e7a76a1925053e13445d55
isnotafunction <ivana.cosgrove@gmail.com>
2018-03-22 12:26:31 +0000

    displays list items

--- public/dom.js
+++ public/dom.js
@@ -1,13 +1,14 @@
 var inputField = document.querySelector(".input");
+var list = document.querySelector('.list')
 
 inputField.addEventListener("keyup", function(e) {
   var userInput = e.target.value;
   if (userInput.length == 1) {
-    request(userInput);
+    request(userInput, display);
   }
 });
 
-var request = function(param) {
+var request = function(param, callback) {
   var xhr = new XMLHttpRequest();
   var url = "/api/phobias/" + param;
   xhr.onreadystatechange = function() {
@@ -15,6 +16,7 @@ var request = function(param) {
       if (xhr.status === 200) {
         var response = JSON.parse(xhr.responseText);
         console.log(response);
+        callback(response);
       } else {
         console.log("error");
       }
@@ -23,3 +25,17 @@ var request = function(param) {
   xhr.open("GET", url, true);
   xhr.send();
 };
+
+function display(data){
+
+  var keys = Object.keys(data)
+  keys.forEach(function(val){
+    var item = document.createElement('li')
+    item.setAttribute('class', 'list__item')
+    var text = document.createTextNode(val)
+    item.appendChild(text)
+    list.appendChild(item)
+  })
+
+
+}
